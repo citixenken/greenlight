b028450418494b693b47f3edcc44884cf03e6882
Ken Muyesu <ken.muyesu@ncbagroup.com>
2025-07-26 08:44:12 +0300

    chore: creating a new movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	"github.com/citixenken/greenlight/internal/validator"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -40,7 +41,14 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year,movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
